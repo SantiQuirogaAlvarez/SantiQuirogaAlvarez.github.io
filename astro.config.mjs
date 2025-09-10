ee8063227b5eecb3e66e3032111cf35541496c24
Santi Quiroga <saqu6490@colorado.edu>
2025-09-09 18:35:08 -0600

    fixed dynamic loading, now only lowercase <filename>.md blogs are compatible.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/', // Use '/' for local dev, set to '/SantiQuirogaAlvarez.github.io/' for GitHub Pages
   vite: {
     plugins: [tailwindcss()],
   }
---
+++ src/content/blog/myfirstblog.md
@@ -0,0 +1,7 @@
+---
+title: "Why I'm building this website"
+description: "Why I decided all of a suden to try and do this the long and hard way"
+date: "2025-09-08"
+tags: ["WebDev", "Ramble"]
+---
+# EL PEPE
